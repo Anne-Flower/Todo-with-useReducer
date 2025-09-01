d0b75df543a52ba5d9e5f2e5b10bad0ff2488a67
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-09-01 08:55:10 +0200

    add function reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  type Todo = {
+    id: number;
+    text: string;
+    check: boolean;
+  };
+
+  type Action =
+    | { type: "add"; id: number; text: string }
+    | { type: "delete"; id: number }
+    | { type: "toggle"; id: number }
+    | { type: "changeText"; id: number; text: string };
+
+  type State = Todo[];
+
+  function reducer(state: State, action: Action): State {
+    switch (action.type) {
+      case "add":
+        return [...state, { id: action.id, text: action.text, check: false }];
+      case "delete":
+        return state.filter((todo) => todo.id !== action.id);
+      case "toggle":
+        return state.map((todo) =>
+          todo.id === action.id ? { ...todo, check: todo.check } : todo
+        );
+      case "changeText":
+        return state.map((todo) =>
+          todo.id === action.id ? { ...todo, text: todo.text } : todo
+        );
+      default:
+        return state;
+    }
+  }
+
+
+  const initialState: State = [];
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return    <div className="TodoList">
+
+  </div>;
+  
+
 }
 
 export default App;
