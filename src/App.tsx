e82b76de2fa0110b826f06ee96e73276ad60ee68
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-09-07 17:42:52 +0200

    add return, todo is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import { useReducer, useState } from "react";
 
 function App() {
   type Todo = {
     id: number;
     text: string;
-    check: boolean;
+    checked: boolean;
   };
 
   type Action =
@@ -18,12 +19,12 @@ function App() {
   function reducer(state: State, action: Action): State {
     switch (action.type) {
       case "add":
-        return [...state, { id: action.id, text: action.text, check: false }];
+        return [...state, { id: action.id, text: action.text, checked: false }];
       case "delete":
         return state.filter((todo) => todo.id !== action.id);
       case "toggle":
         return state.map((todo) =>
-          todo.id === action.id ? { ...todo, check: todo.check } : todo
+          todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
         );
       case "changeText":
         return state.map((todo) =>
@@ -34,15 +35,50 @@ function App() {
     }
   }
 
-
   const initialState: State = [];
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [text, setText] = useState("");
+  const addText = () => {
+    if (text.trim()) {
+      dispatch({ type: "add", text, id: Date.now() });
+      setText("");
+    }
+  };
 
-  return    <div className="TodoList">
+  return (
+    <>
+      <div className="TodoList">
+        <h1>
+          <strong>TODO LIST</strong>
+        </h1>
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter a task here"
+        />
+        <button onClick={addText}> Add </button>
 
-  </div>;
-  
-
+        <ul>
+          {state.map((todo) => (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.checked}
+                  onChange={() => dispatch({ type: "toggle", id: todo.id })}
+                ></input>
+                {todo.text}
+              </label>
+              <button onClick={() => dispatch({ type: "delete", id: todo.id })}>
+                {" "}
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
